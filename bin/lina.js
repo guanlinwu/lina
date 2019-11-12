7db35c0155bc25e3ba0cba6aead858090ec12a37
lgn <lgnsg@foxmail.com>
2019-11-12 15:01:01 +0800

    Merge branch 'master' of F:\lina with conflicts.

--- bin/lina.js
+++ bin/lina.js
@@ -10,7 +10,7 @@ console.log(
 )
 
 const argv = require('yargs')
-  .commandDir('../cmds')
+  .commandDir('../cmds', { recurse: true }/*  递归查找命令行 */)
   .alias('h', 'help')
   .alias('v', 'version')
   .help()
--- cmds/package_cmds/pull.js
+++ cmds/package_cmds/pull.js
@@ -1,10 +1,18 @@
 const shell = require('shelljs')
-
-exports.command = 'pull <name>'
-exports.desc = 'Add remote named <name>'
+exports.command = 'pull <pkgName>'
+exports.desc = 'pull specific packages <pkgName> from remote repository'
 exports.builder = {
 }
 exports.handler = function (argv) {
-  shell.exec('git clone https://github.com/guanlinwu/lina-ui.git')
-  console.log(2)
+  // 判断是否有安装git
+  if (!shell.which('git')) {
+    shell.echo('Sorry, this script requires git')
+    process.exit(1) // 强制退出
+  }
+  // 判断git版本号,必须大于1.7.0
+  const gitVersionReg = /(\d\.\d{1,3}\.\d{1,3})/g
+  let gitVersionResult = shell.exec('git --version').stdout.match(gitVersionReg)
+  if (gitVersionResult) {
+    lgitVersionResult[0].split('.')
+  }
 }
