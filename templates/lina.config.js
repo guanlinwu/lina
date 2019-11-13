fbca5094e7d9e36763d4e08593226a99d30e8f00
wuguanlin <403786672@qq.com>
2019-11-13 15:14:02 +0800

    feat: 优化

--- templates/lina.config.js
+++ templates/lina.config.js
@@ -4,19 +4,19 @@ module.exports = {
   // subPackages: [],
   dependencies: [
     {
-      // repo: '',
-      // src: 'dist',
-      //
+      repo: 'https://github.com/guanlinwu/lina-ui.git',
+      alias: 'lina', // lina package pull [组件] [git-alias], 这时候，git-alias = lina
+      src: 'src/packages', // 仓库的路径
       dest: '<%= dest %>', // 该模块存放的开发目录
       items: [ // 需要更新的模块
-        {
-          'version': '1.0.0', // 组件版本
-          'name': 'Toast'
-        },
-        {
-          'version': '1.0.0', // 组件版本
-          'name': 'Loading'
-        }
+        // {
+        //   'version': '1.0.0', // 组件版本
+        //   'name': 'Toast'
+        // },
+        // {
+        //   'version': '1.0.0', // 组件版本
+        //   'name': 'Loading'
+        // }
       ]
     }
   ]
