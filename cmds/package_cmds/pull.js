8f5dfc5c40193a36e65298e35cd864d1be78fddf
lgn <lgnsg@foxmail.com>
2019-11-13 11:46:01 +0800

    添加拉取文件

--- cmds/package_cmds/pull.js
+++ cmds/package_cmds/pull.js
@@ -21,7 +21,7 @@ exports.handler = function (argv) {
   // 判断git版本号,必须大于1.7.0
   const gitVersionReg = /(\d\.\d{1,4}\.\d{1,4})/g
   let gitVersionResult = shell.exec('git --version', {silent: true}).stdout.match(gitVersionReg)
-  if (!gitVersionResult) {
+  if (gitVersionResult) {
     if (!semver.gt(gitVersionResult[0], '1.7.0')) {
       console.log('\u001b[31m your git version is too low,please update\u001b[0m')
       console.log('\u001b[32m click here to download https://git-scm.com\u001b[0m')
@@ -29,13 +29,47 @@ exports.handler = function (argv) {
     }
   }
   // 执行拉取文件夹操作
-  mkdir('-p', './lina-packages')
+  // 检查
+  !fs.existsSync('./lina-packages') && mkdir('-p', './lina-packages')
   cd('./lina-packages')
-  pwd()
-  exec('git init', {silent: true, async: false})
-  exec(`git remote add origin ${linaRepository}`, {silent: true, async: false})
-  exec('git config core.sparsecheckout true', {async: false})
-  // echo /languages/ >> .git/info/sparse-checkout
-  fs.writeFileSync('.git/info/sparse-checkout', `src/packages/${argv.pkgName}`)
-  exec(`git pull origin master`, {async: false})
-}
+  if (!fs.existsSync('.git')) {
+    pullPkg()
+  } else {
+    delDirectory('.git')
+    pullPkg()
+  }
+  /*
+  * 执行git 拉取操作
+  * */
+
+  function  pullPkg() {
+    shell.echo('current path:', pwd())
+    exec('git init', { silent: true, async: false })
+    exec(`git remote add origin ${linaRepository}`, { silent: true, async: false })
+    exec('git config core.sparsecheckout true', { async: false })
+    // echo /languages/ >> .git/info/sparse-checkout
+    fs.writeFileSync('.git/info/sparse-checkout', `src/packages/${argv.pkgName}`)
+    exec(`git pull origin master`, { async: false })
+  }
+
+  /*
+  * 删除目录
+  * */
+  function delDirectory (dir) {
+    // 此处不用判断不存在路径，前面已经做判断
+    const dirInfo = fs.statSync(dir)
+    if (dirInfo.isDirectory()) { // 目录
+      const dirData = fs.readdirSync(dir) // 目录数据
+      if(dirData.length > 0) {
+        for ( let i = 0; i< dirData.length; i++){ // 好像for 比 forEach会好点, 不过应该没差别
+          delDirectory(`${dir}/${dirData[i]}`) // 这里递归调用，因为它是目录
+          dirData.length-1 === i && delDirectory(dir) // 删除完该目录里面的内容之后，需要把这个目录删了
+        }
+      }else {
+        fs.rmdirSync(dir) // 空目录
+      }
+    } else if(dirInfo.isFile()){
+      fs.unlinkSync(dir) // 删除文件
+    }
+  }
+  }
