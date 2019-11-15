af133f83b347b5f466b173698ccd53cedd998a8c
liguonan <liguonan@inrice.com>
2019-11-15 18:49:17 +0800

    feat: add directions

--- cmds/package.js
+++ cmds/package.js
@@ -1,5 +1,5 @@
-exports.command = 'package <command>'
-exports.description = 'Manage package'
+exports.command = 'package pull <packageName>'
+exports.description = 'pull package(s) from specified Repository '
 exports.builder = function (yargs) {
   return yargs.commandDir('package_cmds')
 }
