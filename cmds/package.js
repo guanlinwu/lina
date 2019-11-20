269d6f171c0d9bb5062185474cf23320dd26aec2
lgn <lgnsg@foxmail.com>
2019-11-20 17:46:10 +0800

    test: :lipstick: 更新readme

--- cmds/package.js
+++ cmds/package.js
@@ -1,5 +1,5 @@
-exports.command = 'package pull <packageName>'
-exports.description = 'Pull package(s) from specified Repository '
+exports.command = 'pull [pkgName] [--git-alias]'
+exports.description = 'pull specific package(s) from remote repository'
 exports.builder = function (yargs) {
   return yargs.commandDir('package_cmds')
 }
--- cmds/package_cmds/pull.js
+++ cmds/package_cmds/pull.js
@@ -5,8 +5,8 @@ const initConfig = require('../../cores/init-config.js') // 初始化逻辑
 const configFileName = 'lina.config.js' // 配置文件名称
 const { HandlePull } = require('../../utils/handlePull')
 
-exports.command = 'pull [pkgName] [git-alias]'
-exports.desc = 'pull specific packages [pkgName] [--git-alias] from remote repository'
+exports.command = 'pull [pkgName] [--git-alias]'
+exports.desc = 'pull specific packages from remote repository'
 exports.builder = {
   pkgName: {
     default: ''
